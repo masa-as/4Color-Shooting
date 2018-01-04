cca91ce7096c8078ca133f6d8d20d9f0dd7c0b62
Masashi Iijima <masashiiijima@gmail.com>
2018-01-04 18:04:25 +0900

    button_disp編集

--- Assets/Button_disp.cs
+++ Assets/Button_disp.cs
@@ -17,7 +17,7 @@ public class Button_disp : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButton("Fire2")||Input.GetKey("z"))
         {
             a.SetActive(true);
         }
